a1d7c109ef101ca08e40c5e887508cc51fa37dfb
Nimah <busayonimota1603@gmail.com>
2022-09-27 01:45:08 +0100

    [Feats]: Create a custom header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,17 @@
 # Create a custom header with puppet
+
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
+}
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
 exec { 'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
