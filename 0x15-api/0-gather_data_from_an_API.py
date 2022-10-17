629d0c7ce9a9983f6aa8992bcf3f2d3e1928300d
Nimah <busayonimota1603@gmail.com>
2022-10-17 17:01:40 +0100

    [Feats]: Fetch employees to do list and completed tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Displays the TODO list progress of employee whose
+ID is passed as a parameter to the script"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    try:
+        response = requests.get("https://jsonplaceholder.typicode.com/users")
+        response = response.json()
+        emp_id = int(sys.argv[1])
+        list_id = emp_id - 1
+        response = response[list_id]
+
+        tasks = requests.get("https://jsonplaceholder.typicode.com/todos")
+        tasks = tasks.json()
+        countTask, countComp = 0, 0
+        compTask = list()
+
+        for dic in tasks:
+            if int(dic.get('userId')) == emp_id:
+                countTask += 1
+                if dic.get('completed') == True:
+                    countComp += 1
+                    tk = dic.get('title')
+                    compTask.append(tk)
+
+        print("Employee", response['name'],
+              "is done with tasks({}/{}):".format(countComp, countTask))
+        for i in compTask:
+            print("\t", i)
+
+    except Exception as err:
+        print("Error: {}".format(err))
